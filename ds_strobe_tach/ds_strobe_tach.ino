ab06f4be64af2e9a5b52b8c5fe8c89a8002fda81
David <ninetreesdesign@users.noreply.github.com>
2020-12-29 14:44:37 -0800

    increased resolution of fine-tune pot, clarified code comments

--- ds_strobe_tach/ds_strobe_tach.ino
+++ ds_strobe_tach/ds_strobe_tach.ino
@@ -1,35 +1,43 @@
-// Use Teensy as a 2 MHz clock source
-// See PWM and resolution at https://www.pjrc.com/teensy/td_pulse.html
-// be sure to use PWM-able pins 3 4 5 6 9... on T3.x
+/*  Use Teensy to make a strobe tachometer
+ *  Use adjustable input (dual pot) to set a frequency in RPM
+ *    display frequency
+ *    flash LEDs in short pulse for strobe
+ *
+ *  See PWM and resolution at https://www.pjrc.com/teensy/td_pulse.html
+ *  be sure to use PWM-able pins 3 4 5 6 9... on T3.x
+ *
+ *  To generate pulse, use low-freq PWM method (couldn't get to work; changed to calculated software pulse
+ *
+*/
 
 #define HWSERIAL Serial1
 #define CONSOLE  Serial                 // USB port debug console (IDE)
 #define E        echoString             // my little print shortcut
 
 /////////////////////////////////////////////////////////////////////////////////////
-const String VERSION_NUM = "1.0.0";     // release version
+const String VERSION_NUM = "1.0.1";     // increased to 0.1rpm resolution
 const bool PRINT_FLAG = 1;              // enable print of streaming data (in echoString)
 const bool HWSERIAL_FLAG = 0;           // send msgs to hardware serial port
 const bool CONSOLE_FLAG = 1;            // send msgs to standard USB output port
 /////////////////////////////////////////////////////////////////////////////////////
 
-byte CLOCK_PIN = 5;
-byte CLOCK_PIN2 = 6;
+byte CLOCK_PIN_A = 5;
+byte CLOCK_PIN_B = 6;
 byte LED_PIN = 13;
-int ch_coarse = A7;
-int ch_fine = A8;
+byte ch_coarse = A7;
+byte ch_fine = A8;
 const float MAX_VAL_ANALOG = 65535.0;
-const float MAX_RPM = 1000;              // sets range of adjustment
+const float MAX_RPM = 400;  // sets range of adjustment
 // intervals for various timed events
-const uint32_t SENSORS_INTERVAL = 100;      // ms
-const uint32_t PRINT_INTERVAL = 200;        // ms
-const uint32_t LED_INTERVAL = 1000;         // ms
+const uint16_t SENSORS_INTERVAL = 100;      // ms
+const uint16_t PRINT_INTERVAL = 200;        // ms
+const uint16_t LED_INTERVAL = 1000;         // ms
 char msg[80];
 float f_freq = 1.00;     // very slow
 float f_rpm = f_freq * 60;
-int t_on;
-int t_off;
-int t0;
+uint16_t t_on;
+uint16_t t_off;
+uint32_t t0;
 bool clock_flag = 0;
 // timer variables
 elapsedMillis since_sensors = 0;        // ms
@@ -38,64 +46,70 @@ elapsedMillis since_led = 0;            // ms
 elapsedMillis since_pulse = 0;          // ms
 
 void setup() {
-    pinMode(CLOCK_PIN, OUTPUT);
-    pinMode(CLOCK_PIN2, OUTPUT);
+    pinMode(CLOCK_PIN_A, OUTPUT);
+    pinMode(CLOCK_PIN_B, OUTPUT);
     pinMode(LED_PIN, OUTPUT);
+    // LED startup splash
     for (int i=0; i<3; i++) {
-        digitalWrite(LED_PIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-        digitalWrite(CLOCK_PIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-        delay(50);               // wait for a time
-        digitalWrite(LED_PIN, LOW);    // turn the LED off by making the voltage LOW
-        digitalWrite(CLOCK_PIN, LOW);    // turn the LED off by making the voltage LOW
-        delay(300);               // wait for a time
+        digitalWrite(LED_PIN, HIGH);    // turn the LED on (HIGH is the voltage level)
+        digitalWrite(CLOCK_PIN_A, HIGH);  // turn the LED on (HIGH is the voltage level)
+        delay(50);                      // wait for a time
+        digitalWrite(LED_PIN, LOW);     // turn the LED off by making the voltage LOW
+        digitalWrite(CLOCK_PIN_A, LOW);   // turn the LED off by making the voltage LOW
+        delay(300);                     // wait for a time
     }
     digitalWrite(LED_PIN,1);
-    analogWriteFrequency(CLOCK_PIN, 50000);	// set PWM freq
+    analogWriteFrequency(CLOCK_PIN_A, 50000);    // set PWM freq
     analogReadResolution(16);
     analogWriteResolution(8);  //
 
-    analogWrite(CLOCK_PIN,127);    // 50%
+    // check PWM change in brightness
+    analogWrite(CLOCK_PIN_A,127);   // 50%
     delay(250);
-    analogWrite(CLOCK_PIN,255);    // 50%
+    analogWrite(CLOCK_PIN_A,64);   // 100%
     delay(250);
-    analogWrite(CLOCK_PIN,10);    // 50%
+    analogWrite(CLOCK_PIN_A,10);    // 10%
     delay(250);
-    analogWrite(CLOCK_PIN,0);    // 0%
+    analogWrite(CLOCK_PIN_A,0);     // 0%
     delay(250);
-
+    // print header message
     E("Strobe Tachometer \n");
     E("Version " + VERSION_NUM + "\n");
+
     t0 = millis();
 }
 
 // --- main loop --------------------------------------------------------------------------------
 void loop() {
+    // get next sensor readings set
+
     static float v_coarse = 0;
     static float v_fine   = 0;
-    float f_period;
-    // get next sensor readings set
+    float f_period = 1000;
+    static float duty_cycle = 0.05;             // 0.10 = 10%
     static float f_rpm_prev = 0;
     if (since_sensors >= SENSORS_INTERVAL) {
         since_sensors = 0;
 
         v_coarse = (MAX_RPM*(float)readPot(ch_coarse) / MAX_VAL_ANALOG);
-        v_fine   =  (MAX_RPM/10*(float)readPot(ch_fine) / MAX_VAL_ANALOG - MAX_RPM/10/2);
+        v_fine   =  (MAX_RPM/100*(float)readPot(ch_fine) / MAX_VAL_ANALOG - MAX_RPM/100/2);
         f_rpm_prev = f_rpm;
-        f_rpm = (float)(int)(v_coarse + v_fine);
-        if (f_rpm < 10.0) f_rpm = 10.0;
-        f_freq = f_rpm/60.0;
-        f_period = 1 / f_freq * 1000;
-        static float duty_cycle = 0.05;             // 0.10 = 10%
+        f_rpm = (float)(0.1*((int)(10*(v_coarse + v_fine))));
+        f_rpm = constrain(f_rpm,10,5000);
+        f_freq = f_rpm / 60.0;
+        f_period = 1.0 / f_freq * 1000;
+        if(f_period < 5) E("LOW ");  // temp
         if (abs(f_rpm - f_rpm_prev) > 0) {
-            t_on = (int)(duty_cycle * f_period);           // in msec
-            t_off = (int)((1.0 - duty_cycle) * f_period);
+            t_on = (int)(duty_cycle * f_period);            // in msec
+            t_on = constrain(t_on,2,10);                           // keep strobe narrow
+            t_off = (int)(f_period) - t_on +(f_period - (int)f_period < 0.5);       // add one count if truncated
         }
     }
 
     if (since_print >= PRINT_INTERVAL) {
         since_print = 0;
         //sprintf(msg, "  %6.1frpm %6.2fHz  C=%5.1f F=%5.1f\n",f_rpm, f_freq, v_coarse, v_fine);
-        sprintf(msg, "  %5d %3.1f rpm   %6.3f Hz  %7.1f ms [%5u %5u]\n", (millis()-t0)/1000, f_rpm, f_freq, f_period, t_on, t_off);
+        sprintf(msg, "  %5lu %5.1f rpm   %6.3f Hz  %6.1f ms [%5u %5u]\n", (millis()-t0)/1000, f_rpm, f_freq, f_period, t_on, t_off);
         E(msg);
     }
 
@@ -113,13 +127,15 @@ void loop() {
     if (since_pulse >= (t_on + t_off)) {
         since_pulse = 0;
         if (clock_flag == 0) {
-            digitalWriteFast(CLOCK_PIN,1);
+            digitalWriteFast(CLOCK_PIN_A,1);
+            digitalWriteFast(CLOCK_PIN_B,1);
             clock_flag = 1;
         }
     }
     if (since_pulse >= t_on) {
         if (clock_flag == 1) {
-            digitalWriteFast(CLOCK_PIN,0);
+            digitalWriteFast(CLOCK_PIN_A,0);
+            digitalWriteFast(CLOCK_PIN_B,0);
             clock_flag = 0;
         }
     }
@@ -130,8 +146,8 @@ void loop() {
 
 /// read potentiometer
 uint16_t readPot(uint8_t ch) {
-    uint8_t num_readings = 8;
-    uint8_t t_delay_us = 10;
+    uint8_t num_readings = 16;
+    uint8_t t_delay_us = 2;
     uint16_t val = readAndAverage(ch, num_readings, t_delay_us);
     return val;
 }
